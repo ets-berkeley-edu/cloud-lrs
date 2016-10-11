4848f863fb4e9eb478006473a0a0e51745b05ea8
sandeepmjay <sandeep.mjay@gmail.com>
2016-10-11 13:00:09 -0700

    Commit contains configurations to deploy to aws elastic beanstalk

--- deploy/start.sh
+++ deploy/start.sh
@@ -4,7 +4,17 @@
 ./deploy/stop.sh
 
 # The directory where forever logs should be sent to
-LOG_DIR=~/Desktop/ETS/cloud-lrs/logs
+# LOG_DIR=~/Desktop/ETS/cloud-lrs/logs
+
+npm install -g bower
+
+npm install
+
+bower install
+
+cp -R public /var/www/html/ 
 
 # Start the app server
-node_modules/.bin/forever -a -l "$LOG_DIR/forever.log" -i "$LOG_DIR/forever_app.log" -e "$LOG_DIR/forever_app.log" -m 10 start app.js
+
+#node_modules/.bin/forever -a -l "$LOG_DIR/forever.log" -i "$LOG_DIR/forever_app.log" -e "$LOG_DIR/forever_app.log" -m 10 start app.js
+node_modules/.bin/forever -a -m 10 start app.js
